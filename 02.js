874410f80573227e6bfb6e71ab16ccd4c7543470
Miquel Fire Burns <miquelfire@gmail.com>
2024-12-02 10:58:02 -0500

    Day 2 Part 2 done

--- 02.js
+++ 02.js
@@ -1,4 +1,22 @@
 /**
+ * @param {number[]}
+ */
+function checkReport(report) {
+	return report.every(e => Math.abs(e) < 4) && (report.every(e => e > 0) || report.every(e => e < 0));
+}
+
+/**
+ * @param {number[]} levels 
+ */
+function processLevels(levels) {
+	const diffs = [];
+	for (let i = 1; i < levels.length; i++) {
+		diffs.push(levels[i] - levels[i - 1]);
+	}
+	return diffs;
+}
+
+/**
  * @param {string} d 
  */
 export const part1 = async d => {
@@ -6,13 +24,13 @@ export const part1 = async d => {
 	const diffs = [];
 	for (let i = 0; i < reports.length; i++) {
 		const levelDiffs = [];
-		const report = reports[i]
+		const report = reports[i];
 		for (let j = 1; j < report.length; j++) {
 			levelDiffs.push(report[j] - report[j - 1]);
 		}
 		diffs.push(levelDiffs);
 	}
-	const temp = diffs.filter(e => e.every(e => Math.abs(e) < 4) && (e.every(e => e > 0) || e.every(e => e < 0)));
+	const temp = diffs.filter(e => checkReport(e));
 	return temp.length;
 };
 
@@ -20,7 +38,20 @@ export const part1 = async d => {
  * @param {string} d 
  */
 export const part2 = async d => {
-	const data = d.split('\n');
-	data.splice(0, data.length);
-	return data;
+	const reports = d.split('\n').map(e => e.split(' '));
+	const safeReports = [];
+	reports.forEach(e => {
+		if (checkReport(processLevels(e))) {
+			safeReports.push(processLevels(e));
+			return;
+		}
+		for (let i = 0; i < e.length; i++) {
+			const temp = e.filter((e, j) => i != j);
+			if (checkReport(processLevels(temp))) {
+				safeReports.push(processLevels(temp));
+				return;
+			}
+		}
+	});
+	return safeReports.length;
 };
