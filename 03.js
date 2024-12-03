02046eb4c9841f8ecf78140006380a73973821f2
Miquel Fire Burns <miquelfire@gmail.com>
2024-12-03 12:08:44 -0500

    Day 3 Part 2 Done

--- 03.js
+++ 03.js
@@ -16,6 +16,25 @@ export const part1 = async d => {
  * @param {string} d 
  */
 export const part2 = async d => {
-	const data = d;
-	return data;
+	const regex = /mul\((\d{1,3}),(\d{1,3})\)|do(?:n't)?\(\)/g;
+	let data = regex.exec(d);
+	let enabled = true;
+	let sum = 0;
+	while (data != null) {
+		switch (data[0]) {
+			case 'do()':
+				enabled = true;
+				break;
+			case 'don\'t()':
+				enabled = false;
+				break;
+			default:
+				if (enabled) {
+					sum += data[1] * data[2];
+				}
+				break;
+		}
+		data = regex.exec(d);
+	}
+	return sum;
 };
