31595fc590a4498bf495a8ad7ca2eca0a44a090e
Miquel Fire Burns <miquelfire@gmail.com>
2024-12-04 23:39:27 -0500

    Day 4 Part 2 Done

--- 04.js
+++ 04.js
@@ -51,7 +51,7 @@ export const part1 = async d => {
 						sum++;
 					}
 				}
-				if (x > 2 && y >2) {
+				if (x > 2 && y > 2) {
 					// Check Up Left
 					if (data[x - 1][y - 1] == 'M' && data[x - 2][y - 2] == 'A' && data[x - 3][y - 3] == 'S') {
 						sum++;
@@ -68,7 +68,25 @@ export const part1 = async d => {
  * @param {string} d 
  */
 export const part2 = async d => {
-	const data = d.split('\n');
-	data.splice(0, data.length);
-	return data;
+	const data = d.split('\n').map(e => e.split(''));
+	let sum = 0;
+	for (let x = 1; x < data.length - 1; x++) {
+		for (let y = 1; y < data[x].length - 1; y++) {
+			if (data[x][y] == 'A') {
+				if (data[x - 1][y - 1] == 'M' && data[x + 1][y + 1] == 'S' && data[x + 1][y - 1] == 'M' && data[x - 1][y + 1] == 'S') {
+					sum++;
+				}
+				if (data[x + 1][y + 1] == 'M' && data[x - 1][y - 1] == 'S' && data[x + 1][y - 1] == 'M' && data[x - 1][y + 1] == 'S') {
+					sum++;
+				}
+				if (data[x + 1][y + 1] == 'M' && data[x - 1][y - 1] == 'S' && data[x - 1][y + 1] == 'M' && data[x + 1][y - 1] == 'S') {
+					sum++;
+				}
+				if (data[x - 1][y - 1] == 'M' && data[x + 1][y + 1] == 'S' && data[x - 1][y + 1] == 'M' && data[x + 1][y - 1] == 'S') {
+					sum++;
+				}
+			}
+		}
+	}
+	return sum;
 };
