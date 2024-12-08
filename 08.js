eb1a8c2ee0665d8e9c470fefc59f5e0b81b5d117
Miquel Fire Burns <miquelfire@gmail.com>
2024-12-08 13:33:45 -0500

    Day 8 Part 2 Done

--- 08.js
+++ 08.js
@@ -33,10 +33,8 @@ export const part1 = async d => {
 				if (-1 < point2[0] && point2[0] < width && -1 < point2[1] && point2[1] < height) {
 					aniNodes.add(point2.join('x'));
 				}
-				//console.log(nodeList[i], nodeList[j], distX, distY, point1, point2);
 			}
 		}
-		//console.log(nodeList);
 	}
 	return aniNodes.size;
 };
@@ -45,7 +43,46 @@ export const part1 = async d => {
  * @param {string} d 
  */
 export const part2 = async d => {
-	const data = d.split('\n');
-	data.splice(0, data.length);
-	return data;
+	const data = d.split('\n').map(e => e.split(''));
+	const height = data.length;
+	const width = data[0].length;
+	/** @type {Map<string, [number, number][]} */
+	const nodes = new Map();
+	/** @type {Set<string>} */
+	const aniNodes = new Set();
+	for (let x = 0; x < width; x++) {
+		for (let y = 0; y < height; y++) {
+			if (data[y][x] != '.') {
+				const nodeLabel = data[y][x];
+				const nodeList = nodes.get(nodeLabel) || [];
+				nodeList.push([x, y]);
+				nodes.set(nodeLabel, nodeList);
+			}
+		}
+	}
+
+	for (const nodeList of nodes.values()) {
+		if (nodeList.length == 1) continue;
+		for (let i = 0; i < nodeList.length - 1; i++) {
+			for (let j = i + 1; j < nodeList.length; j++) {
+				aniNodes.add(nodeList[i].join('x'));
+				aniNodes.add(nodeList[j].join('x'));
+				const distX = nodeList[i][0] - nodeList[j][0];
+				const distY = nodeList[i][1] - nodeList[j][1];
+				const point1 = [nodeList[i][0] + distX, nodeList[i][1] + distY];
+				const point2 = [nodeList[j][0] - distX, nodeList[j][1] - distY];
+				while (-1 < point1[0] && point1[0] < width && -1 < point1[1] && point1[1] < height) {
+					aniNodes.add(point1.join('x'));
+					point1[0] += distX;
+					point1[1] += distY;
+				}
+				while (-1 < point2[0] && point2[0] < width && -1 < point2[1] && point2[1] < height) {
+					aniNodes.add(point2.join('x'));
+					point2[0] -= distX;
+					point2[1] -= distY;
+				}
+			}
+		}
+	}
+	return aniNodes.size;
 };
