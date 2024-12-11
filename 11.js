9abb38010ca1a65da8f8ba5f9b8069ccf5e595d0
Miquel Fire Burns <miquelfire@gmail.com>
2024-12-11 10:56:10 -0500

    I converted PHolder's code to JS because I didn't see how to do this without brute force.

--- 11.js
+++ 11.js
@@ -1,37 +1,97 @@
+class CountKeeper {
+	/** @type {Map<number, number>} */
+	#counts = new Map();
+
+	/**
+	 * 
+	 * @param {number} valueToGetCountOf 
+	 */
+	getCountOf(valueToGetCountOf) {
+		return this.#counts.get(valueToGetCountOf) || 0;
+	}
+
+	/**
+	 * 
+	 * @param {number} valueToIncreaseCountOf 
+	 * @param {number} amountToIncreaseBy 
+	 */
+	increaseCountOf(valueToIncreaseCountOf, amountToIncreaseBy) {
+		if (!this.#counts.has(valueToIncreaseCountOf)) {
+			this.#counts.set(valueToIncreaseCountOf, amountToIncreaseBy);
+		} else {
+			this.#counts.set(valueToIncreaseCountOf, this.#counts.get(valueToIncreaseCountOf) + amountToIncreaseBy);
+		}
+	}
+
+	getCountedValues() {
+		return [...this.#counts.keys()];
+	}
+}
+
 /**
  * @param {string} d 
  */
 export const part1 = async d => {
-	/**
-	 * 
-	 * @param {string[]} stones 
-	 */
-	function handleStones(stones) {
-		for (let i = stones.length - 1; i > -1; i--) {
-			if (stones[i] == '0') {
-				stones[i] = '1';
-			} else if (stones[i].length % 2 == 0) {
-				stones.splice(i + 1, 0, +(stones[i].substring(stones[i].length / 2)) + '');
-				stones[i] = +(stones[i].substring(0, stones[i].length / 2)) + '';
-			} else {
-				stones[i] = stones[i] * 2024 + '';
-			}
-		}
-	}
+	const data = d.split(' ').map(e => +e);
+	let ck = new CountKeeper;
 
-	const data = d.split(' ');
+	data.forEach(e => ck.increaseCountOf(e, 1));
 
 	for (let i = 0; i < 25; i++) {
-		handleStones(data);
+		const newCk = new CountKeeper;
+		ck.getCountedValues().forEach(e => {
+			const numOfValue = ck.getCountOf(e);
+			if (e == 0) {
+				newCk.increaseCountOf(1, numOfValue);
+				return;
+			}
+			const n = Math.trunc(Math.log10(e) + 1);
+			if (n % 2 == 0) {
+				const left = Math.trunc(e / Math.pow(10, n / 2));
+				const right = e % Math.pow(10, n / 2);
+				newCk.increaseCountOf(left, numOfValue);
+				newCk.increaseCountOf(right, numOfValue);
+			} else {
+				newCk.increaseCountOf(e * 2024, numOfValue);
+			}
+		});
+		ck = newCk;
 	}
-	return data.length;
+	let sum = 0;
+	ck.getCountedValues().forEach(e => sum += ck.getCountOf(e));
+	return sum;
 };
 
 /**
  * @param {string} d 
  */
 export const part2 = async d => {
-	const data = d.split(' ');
-	data.splice(0, data.length);
-	return data;
+	const data = d.split(' ').map(e => +e);
+	let ck = new CountKeeper;
+
+	data.forEach(e => ck.increaseCountOf(e, 1));
+
+	for (let i = 0; i < 75; i++) {
+		const newCk = new CountKeeper;
+		ck.getCountedValues().forEach(e => {
+			const numOfValue = ck.getCountOf(e);
+			if (e == 0) {
+				newCk.increaseCountOf(1, numOfValue);
+				return;
+			}
+			const n = Math.trunc(Math.log10(e) + 1);
+			if (n % 2 == 0) {
+				const left = Math.trunc(e / Math.pow(10, n / 2));
+				const right = e % Math.pow(10, n / 2);
+				newCk.increaseCountOf(left, numOfValue);
+				newCk.increaseCountOf(right, numOfValue);
+			} else {
+				newCk.increaseCountOf(e * 2024, numOfValue);
+			}
+		});
+		ck = newCk;
+	}
+	let sum = 0;
+	ck.getCountedValues().forEach(e => sum += ck.getCountOf(e));
+	return sum;
 };
