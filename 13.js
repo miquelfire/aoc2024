9393cc2ba2c7ca04e22e5c48da4aa9cf10b6603d
Miquel Fire Burns <miquelfire@gmail.com>
2024-12-13 16:18:17 -0500

    Day 13 Part 1 Done

---
+++ 13.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} d 
+ */
+export const part1 = async d => {
+	const data = d.split('\n\n').map(e => {
+		const regex = /Button A: X\+(\d+), Y\+(\d+)\n\r?Button B: X\+(\d+), Y\+(\d+)\n\r?Prize: X=(\d+), Y=(\d+)/;
+		const match = regex.exec(e);
+		if (match == null) throw new Error;
+		const [,ax,ay,bx,by,px,py] = match;
+		return [ax,ay,bx,by,px,py];
+	}).map(e => e.map(e => +e));
+	data.forEach((e, i) => {
+		const b = (e[5] * e[0] - e[4] * e[1]) / (e[3] * e[0] - e[2] * e[1]);
+		const a = (e[5] - e[3] * b) / e[1];
+		if (a % 1 == 0 && b % 1 == 0) {
+			data[i] = [a, b];
+		} else {
+			data[i] = false;
+		}
+	});
+	return data.filter((e => e)).reduce((p, v) => p += v[0] * 3 + v[1], 0);
+};
+
+/**
+ * @param {string} d 
+ */
+export const part2 = async d => {
+	const data = d.split('\n');
+	data.splice(0, data.length);
+	return data;
+};
