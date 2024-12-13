8eb132c702493673fbe9130495472d4d90bb8a75
Miquel Fire Burns <miquelfire@gmail.com>
2024-12-13 16:26:05 -0500

    Day 13 Part 2 Done First time I was forced to use BigInt!

--- 13.js
+++ 13.js
@@ -6,8 +6,8 @@ export const part1 = async d => {
 		const regex = /Button A: X\+(\d+), Y\+(\d+)\n\r?Button B: X\+(\d+), Y\+(\d+)\n\r?Prize: X=(\d+), Y=(\d+)/;
 		const match = regex.exec(e);
 		if (match == null) throw new Error;
-		const [,ax,ay,bx,by,px,py] = match;
-		return [ax,ay,bx,by,px,py];
+		const [, ax, ay, bx, by, px, py] = match;
+		return [ax, ay, bx, by, px, py];
 	}).map(e => e.map(e => +e));
 	data.forEach((e, i) => {
 		const b = (e[5] * e[0] - e[4] * e[1]) / (e[3] * e[0] - e[2] * e[1]);
@@ -25,7 +25,23 @@ export const part1 = async d => {
  * @param {string} d 
  */
 export const part2 = async d => {
-	const data = d.split('\n');
-	data.splice(0, data.length);
-	return data;
+	const data = d.split('\n\n').map(e => {
+		const regex = /Button A: X\+(\d+), Y\+(\d+)\n\r?Button B: X\+(\d+), Y\+(\d+)\n\r?Prize: X=(\d+), Y=(\d+)/;
+		const match = regex.exec(e);
+		if (match == null) throw new Error;
+		const [, ax, ay, bx, by, px, py] = match;
+		return [BigInt(ax), BigInt(ay), BigInt(bx), BigInt(by), BigInt(px) + 10000000000000n, BigInt(py) + 10000000000000n];
+	});
+	data.forEach((e, i) => {
+		const b = (e[5] * e[0] - e[4] * e[1]) / (e[3] * e[0] - e[2] * e[1]);
+		const bm = (e[5] * e[0] - e[4] * e[1]) % (e[3] * e[0] - e[2] * e[1]);
+		const a = (e[5] - e[3] * b) / e[1];
+		const am = (e[5] - e[3] * b) % e[1];
+		if (am == 0 && bm == 0) {
+			data[i] = [a, b];
+		} else {
+			data[i] = false;
+		}
+	});
+	return data.filter((e => e)).reduce((p, v) => p += v[0] * 3n + v[1], 0n).toString(10);
 };
