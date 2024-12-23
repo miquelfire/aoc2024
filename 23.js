0f456f4c9e39f85acb694581dd9186da125e4107
Miquel Fire Burns <miquelfire@gmail.com>
2024-12-23 15:39:27 -0500

    Day 23 Done.
    Code I took required lodash

--- 23.js
+++ 23.js
@@ -1,6 +1,8 @@
+import _ from 'lodash';
+
 /**
- * @param {string} d 
- */
+* @param {string} d 
+*/
 export const part1 = async d => {
 	const data = d.split('\n').map(e => e.split('-'));
 	/**@type {Map<string, string[]} */
@@ -35,7 +37,7 @@ export const part1 = async d => {
 			for (let j of connectedNodes) {
 				if (i == j) continue;
 				if (nodes.get(i).includes(j)) {
-					clusters.add([e,i,j].sort().join(','));
+					clusters.add([e, i, j].sort().join(','));
 				}
 			}
 		}
@@ -48,7 +50,44 @@ export const part1 = async d => {
  * @param {string} d 
  */
 export const part2 = async d => {
-	const data = d.split('\n');
-	data.splice(0, data.length);
-	return data;
+	const data = d.split('\n').map(e => e.split('-'));
+	/**@type {Map<string, string[]} */
+	const nodes = new Map();
+	let found = new Set();
+
+	data.forEach(e => {
+		if (!nodes.has(e[0])) {
+			nodes.set(e[0], []);
+		}
+		if (!nodes.has(e[1])) {
+			nodes.set(e[1], []);
+		}
+
+		nodes.get(e[0]).push(e[1]);
+		nodes.get(e[1]).push(e[0]);
+	});
+
+	for (let [key1, conn1] of nodes.entries()) {
+		const cc = [key1, ...conn1];
+		const other = conn1.map((key2) => [key2, ...nodes.get(key2)]);
+
+		const w = other.map((s) => _.intersection(cc, s).length);
+		const maxw = _.max(w);
+		if (w.filter(v => v === maxw).length < maxw - 1) continue;
+		if (maxw < found.size) continue;
+
+		let base = [...cc];
+		for (let i = 0; i < other.length && base.length >= maxw; i++) {
+			if (w[i] !== maxw) continue;
+
+			base = _.intersection(base, other[i]);
+		}
+
+		if (base.length < maxw) continue;
+		const res = new Set(base.sort());
+		if (_.isEqual(found, res)) continue;
+		found = res;
+	}
+
+	return [...found].join(',');
 };
